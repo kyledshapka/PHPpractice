d08020030859feaae7bb8e6e1b355e187ee0a6a7
kyledshapka <kyledshapka@gmail.com>
2017-06-21 12:28:19 -0600

    Lesson 24

---
+++ Lesson 19/controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+class PagesController
+{
+	public function home()
+	{
+		return view( 'index' );
+	}
+
+	public function about()
+	{
+		$company = 'The Crimson Corporation';
+		return view( 'about', [ 'company => $company' ]);
+	}
+
+	public function contact()
+	{
+		$email = 'crimsoncorp@druuge.net';
+		return view( 'contact', [ 'email => $email' ]);
+	}
+}
